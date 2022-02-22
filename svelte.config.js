50327a27c7b0e4e48c84c4a4bc83c0a5a3e20b56
MorningPants <77476821+MorningPants@users.noreply.github.com>
2022-02-22 11:10:35 -0800

    gh pages config

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,39 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	kit: {
+// 		adapter: adapter(),
+
+// 		// Override http methods in the Todo forms
+// 		methodOverride: {
+// 			allowed: ['PATCH', 'DELETE']
+// 		}
+// 	}
+// };
+
+// export default config;
+
+
+
+// import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter(),
+ // Consult https://github.com/sveltejs/svelte-preprocess
+ // for more information about preprocessors
+//  preprocess: preprocess(),
 
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
-	}
+ kit: {
+ // hydrate the <div id="svelte"> element in src/app.html
+//   target: '#svelte',
+  adapter: adapter({
+    pages: 'build',
+    assets: 'build',
+    fallback: null
+  })
+ }
 };
 
 export default config;
